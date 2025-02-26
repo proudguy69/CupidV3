6e777e2d981b9bbb7e6f11c6ae7b59ff65174483
ProudGuy69 <idioticbatteringram@gmail.com>
2025-02-26 18:41:14 -0500

    added comments

--- other/imagegeneration.py
+++ other/imagegeneration.py
@@ -4,16 +4,17 @@ class ImageGenerator():
 
     @classmethod
     def generate_level_card(cls, uesrname, level, rank, xp):
+        # create card, get pfp, size it.
         card = Image.new("RGBA", (int(412*2*1.25), int(412*1.25)), (255, 255, 255, 255)) 
         pfp = Image.open('other/average_discord_user_pfp.png')
         pfp = pfp.resize((212, 212))
         
-        # mask
+        # create a mask to make the pfp circular
         mask = Image.new("L", (212, 212), 0) 
         draw = ImageDraw.Draw(mask)
         draw.ellipse((0, 0, 212, 212), fill=255)  
-
-        pfp.putalpha(mask) 
+        
+        pfp.putalpha(mask)
 
         card.paste(pfp, (50, 50), pfp)
         draw = ImageDraw.Draw(card)
